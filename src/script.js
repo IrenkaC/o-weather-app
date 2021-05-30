dfd8d4535c86cb2166488fd443d3b93d0afe92a1
IrenkaC <35430977+IrenkaC@users.noreply.github.com>
2021-05-30 20:46:50 +0100

    formatDay

--- src/script.js
+++ src/script.js
@@ -20,6 +20,14 @@ let days = [
 let day = days[now.getDay()];
 h3.innerHTML = `${day} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
